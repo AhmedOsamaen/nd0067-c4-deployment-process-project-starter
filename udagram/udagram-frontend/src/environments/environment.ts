c62a5cdf80c8e595cfcb53039c01e91f218893c6
Ahmed Osama <ahmed.osama@datagearbi.com>
2022-12-26 13:19:38 +0200

    fix linting problem

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: "http://udagramfwdapi-env.eba-kcjxruha.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: 'http://udagramfwdapi-env.eba-kcjxruha.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
